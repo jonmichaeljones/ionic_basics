26ee4e31264ba489c1556cff2df7af55a36f1f67
jonmichaeljones <jones.jonmichael@gmail.com>
2016-03-23 19:02:08 +0000

    another commit

--- chapter5/www/js/app.js
+++ chapter5/www/js/app.js
@@ -1,85 +1,84 @@
-// Ionic Starter App
+angular.module('App', ['ionic', 'highcharts-ng'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('tabs', {
+      url: '/tabs',
+      abstract: true,
+      templateUrl: 'views/tabs/tabs.html'
+    })
+    .state('tabs.rates', {
+      url: '/rates',
+      views: {
+        'rates-tab': {
+          templateUrl: 'views/rates/rates.html',
+          controller: 'RatesController'
+        }
+      }
+    })
+    .state('tabs.history', {
+      url: '/history?currency',
+      views: {
+        'history-tab': {
+          templateUrl: 'views/history/history.html',
+          controller: 'HistoryController'
+        }
+      }
+    })
+    .state('tabs.currencies', {
+      url: '/currencies',
+      views: {
+        'currencies-tab': {
+          templateUrl: 'views/currencies/currencies.html',
+          controller: 'CurrenciesController'
+        }
+      }
+    })
+    .state('tabs.detail', {
+      url: '/detail/:currency',
+      views: {
+        'rates-tab': {
+          templateUrl: 'views/detail/detail.html',
+          controller: 'DetailController'
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise('/tabs/rates');
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
     }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
+    if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
+.factory('Currencies', function () {
+  return [
+    { code: 'AUD', text: 'Australian Dollar', selected: true },
+    { code: 'BRL', text: 'Brazilian Real', selected: false },
+    { code: 'CAD', text: 'Canadian Dollar', selected: true },
+    { code: 'CHF', text: 'Swiss Franc', selected: false },
+    { code: 'CNY', text: 'Chinese Yuan', selected: true},
+    { code: 'EUR', text: 'Euro', selected: true },
+    { code: 'GBP', text: 'British Pound Sterling', selected: true },
+    { code: 'IDR', text: 'Indonesian Rupiah', selected: false },
+    { code: 'ILS', text: 'Israeli New Sheqel', selected: false },
+    { code: 'MXN', text: 'Mexican Peso', selected: true },
+    { code: 'NOK', text: 'Norwegian Krone', selected: false },
+    { code: 'NZD', text: 'New Zealand Dollar', selected: false },
+    { code: 'PLN', text: 'Polish Zloty', selected: false },
+    { code: 'RON', text: 'Romanian Leu', selected: false },
+    { code: 'RUB', text: 'Russian Ruble', selected: true },
+    { code: 'SEK', text: 'Swedish Krona', selected: false },
+    { code: 'SGD', text: 'Singapore Dollar', selected: false },
+    { code: 'USD', text: 'United States Dollar', selected: true },
+    { code: 'ZAR', text: 'South African Rand', selected: false }
+  ];
 });
